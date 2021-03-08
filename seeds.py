3fa4a9368aeef1da04f0c5b78d0f556caa667d38
Mark <m.matthewman@gmail.com>
2021-03-08 10:43:26 +0100

    feat: create csv file for multiple runs
    ROOT files may contain multiple runs. seeds.py allows to extract HLT-L1 information from multiple HLT menus and outputs a csv file containing all runs

--- seeds.py
+++ seeds.py
@@ -1,50 +1,78 @@
 import os
 import csv
 
-run = "325001"
+dir = "data/runs/"
+files = os.listdir(dir)
 
-with open (run+".py","r") as f:
-    data = f.read()
+runs = {}
 
-# Create dictionary with HLT paths as key and hltL1s objects as values
+for file in files:
+    with open(dir + file,"r") as f:
+        
+        data = f.read()
 
-HLT_paths={}
-for item in data.split('\n'):
-    if "cms.Path(" in item and "process.hltL1s" in item:
-        hlt = item.split("_v")[0].split("process.")[1]
-        HLT_paths[hlt] = []
-        temp = item.split("process.hltL1s")
-        for i in range(1,len(temp)):
-            l1 = item.split("process.hltL1s")[i].split(" +")[0]
-            HLT_paths[hlt].append(l1)
+    # Create dictionary with HLT paths as key and hltL1s objects as values
 
-# hltL1s combine multiple L1 seeds. Create mapping between hltl1s objects to all L1 seeds
+    HLT_paths={}
+    for item in data.split('\n'):
+        if "cms.Path(" in item and "process.hltL1s" in item:
+            hlt = item.split("_v")[0].split("process.")[1]
+            HLT_paths[hlt] = []
+            temp = item.split("process.hltL1s")
+            for i in range(1,len(temp)): # An HLT path can contain multiple hltL1s structures
+                l1 = item.split("process.hltL1s")[i].split(" +")[0]
+                HLT_paths[hlt].append(l1)
 
-L1_seeds={}
-new = data.split("\n)\n")
+    # hltL1s combine multiple L1 seeds. Create mapping between hltl1s objects to all L1 seeds
 
-for ele in new:
-    if " = cms.EDFilter" in ele:
-        for seed in HLT_paths.values():
-            for ding in seed:
-                if ding in ele and "cms.string( " in ele:
-                    L1_seeds[ding] = ele.split('cms.string( "')[1].split('" ),\n')[0]
+    L1_seeds={}
+    for ele in data.split("\n)\n"):
+        if " = cms.EDFilter" in ele:
+            for seed in HLT_paths.values():
+                for ding in seed:
+                    if ding in ele and "cms.string( " in ele:
+                        L1_seeds[ding] = ele.split('cms.string( "')[1].split('" ),\n')[0]
 
-# Create dictionary mapping HLT paths to L1 seeds
+    # Create dictionary mapping HLT paths to L1 seeds
 
-combined = {}
-for key in HLT_paths.keys():
-    combined[key] = []
-    for path in HLT_paths[key]:
-        try:
-            combined[key].append(L1_seeds[path])
-        except:
-            pass
+    combined = {}
+    for key in HLT_paths.keys():
+        if key not in combined.keys():
+            combined[key] = []
+        for path in HLT_paths[key]:
+            try:
+                combined[key].append(L1_seeds[path])
+            except:
+                pass
 
-with open("test_"+run+".csv","w") as csv_file:
+    runs[file.split(".py")[0]] = combined
+
+keys = set(runs[runs.keys()[1]].keys())|set(runs[runs.keys()[0]].keys())
+
+if not os.path.exists("output"):
+    os.mkdir("output")
+
+with open("output/test.csv","w") as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(["HLT","Seed"])
-    for key in combined.keys():
-        for ele in combined[key]:
-            writer.writerow([key,ele])
+    writer.writerow(["HLT"] + runs.keys())
 
+    for key in keys:
+        # Count the max number of hltL1s structures for single HLT path
+        ls = []
+        for run in runs.keys():
+            try:
+                ls.append(len(runs[run][key]))
+            except KeyError:
+                ls.append(len([]))
+        m = max(ls)
+
+        for i in range(m): # Iterate through all seeds in possible multiple hltL1s structures for a single HLT path
+            ls = []
+            ls.append(key)
+            for run in runs.keys():
+                try:
+                    ls.append(runs[run][key][i])
+                except KeyError:
+                    ls.append(None)
+
+            writer.writerow(ls)
